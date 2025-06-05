e28d9dd21241eb0ac44cfb42b92b40bc5b58d5d9
okokbury <talinhasjv@gmail.com>
2025-06-05 17:29:10 -0300

    Ajuste tela inicial, criacao de controller, ajuste de responsividade

--- routes/web.php
+++ routes/web.php
@@ -3,21 +3,17 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\UserController;
-use App\Http\Controllers\EstoqueController;
+use App\Http\Controllers\OrdemServicoController;
 
 Route::get('/usuarios', [UserController::class, 'index']);
 Route::get('/usuarios/criarfuncionario', [UserController::class, 'create_funcionario']);
 Route::get('/usuarios/criarcliente', [UserController::class, 'create_cliente']);
 
-Route::get('/estoque', [EstoqueController::class, 'index']);
+Route::get('/ordemservico', [OrdemServicoController::class, 'index']);
 
 Route::get('/produtos', [ProductController::class, 'index']);
 Route::get('/produtos/criar', [ProductController::class, 'create']);
 
 Route::get('/', function () {
-    return view('inicio');
-});
-
-Route::get('/homepage', function () {
     return view('homepage');
 });
